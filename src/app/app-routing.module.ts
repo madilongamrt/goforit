34f73d65c52cffda1a8d36b636e2e831b2d09097
Rudzani Matshothsi <rudzani@thedigitalacademy.co.za>
2019-11-08 12:35:22 +0200

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,15 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
+  { path: 'dashboard', loadChildren: './pages/dashboard/dashboard.module#DashboardPageModule' },
+  { path: 'details/:myid', loadChildren: './pages/details/details.module#DetailsPageModule' },
+  { path: 'tabs', loadChildren: './pages/tabs/tabs.module#TabsPageModule' },
+  { path: 'maps', loadChildren: './pages/maps/maps.module#MapsPageModule' },
+  { path: 'list', loadChildren: './pages/list/list.module#ListPageModule' },
+  { path: 'sugest', loadChildren: './pages/sugest/sugest.module#SugestPageModule' },
+  { path: 'register', loadChildren: './pages/register/register.module#RegisterPageModule' },
 ];
 
 @NgModule({
---
+++ src/app/pages/sugest/sugest.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sugest',
+  templateUrl: './sugest.page.html',
+  styleUrls: ['./sugest.page.scss'],
+})
+export class SugestPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
